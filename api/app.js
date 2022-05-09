d182c81f27779b14e109b9a30491f511b5770663
Ramira10 <matiasramira11@hotmail.com>
2022-05-09 09:54:30 -0300

    fixes

--- api/app.js
+++ api/app.js
@@ -18,11 +18,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 // Para manejar errores con next(error)
-// app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-//     const status = err.status || 500;
-//     const message = err.message || err;
-//     console.error(err);
-//     res.status(status).send(message);
-//   });
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
 
 module.exports = app;
